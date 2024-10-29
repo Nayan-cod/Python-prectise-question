1186b1bcd373b0e7740629f69f16d8653db3840c
Nayan-cod <143474149+Nayan-cod@users.noreply.github.com>
2024-10-29 23:47:48 +0530

    Add files via upload

---
+++ 30thjan.py
@@ -0,0 +1,17 @@
+#a=int(input("enter the number:"
+n=5
+i=1
+while(i<=n):
+    j=n-i
+    while(j>0):
+
+        print(" ",end=" ")
+        j-=1
+    k=1
+    while k<=(2*i-1):
+        print("*",end=" ")
+        k+=1
+    print()
+    i+=1
+   
+
